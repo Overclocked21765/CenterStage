011aac4fe789ca8cb9e28888204d7b7903cceaaf
rocketbooster1000 <chadpowers444@gmail.com>
2023-10-14 10:08:15 -0600

    soup workshop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/example/PIDDemonstration.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.example;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class PIDDemonstration extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
