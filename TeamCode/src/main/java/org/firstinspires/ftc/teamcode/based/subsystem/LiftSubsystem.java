59798a97bbc092b7df23ca64a4a4176586f055e1
Jason <118124215+rocketbooster1000@users.noreply.github.com>
2023-12-08 22:42:00 -0700

    config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/based/subsystem/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/based/subsystem/LiftSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.based.subsystem;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.common.RobotHardwareConfig;
 
+@Config
 public class LiftSubsystem extends SubsystemBase {
     private DcMotorEx liftMotor;
 
